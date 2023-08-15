2ef0a3a929dff039775898e039a23b0aa88cb5bb
Heibrancelo <s223529731@deakin.edu.au>
2023-08-15 23:52:25 +1000

    Step 1

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-function changeText() {
-  var textsArray = [
-    "Change",
-    "Changing",
-    "Changed",
-    "Not cháńgíńg",
-    "Chaaaaaaange",
-  ];
-  var number = getRandomNumberBetween(0, textsArray.length - 1);
-  console.log("Index: ", number);
-  document.getElementById("heading").innerHTML = textsArray[number];
-}
-function getRandomNumberBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function getSum() {
-  const num1 = document.getElementById("num1").value;
-  const num2 = document.getElementById("num2").value;
-
-  fetch(`http://localhost:3000/add?num1=${num1}&num2=${num2}`)
-    .then((response) => response.text())
-    .then((result) => {
-      document.getElementById("sum").textContent = result;
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!")
+    }
+    $(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#clickMeButton').click(()=>{
+    clickMe();
+    })
     });
-}
+    
